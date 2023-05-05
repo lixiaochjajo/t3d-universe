72974cd6d8bd97cfc9a99d74e8af81941005e971
lixiaochao <lixiaochao@uino.com>
2023-05-05 16:10:22 +0800

    fix build error

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build:{
+    target:['edge90','chrome90','firefox90','safari15']
+  }
 })
